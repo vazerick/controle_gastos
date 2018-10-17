48aba9c1b2e0999ab5e36506b685be629426482f
Erick Vaz <erickvaz.rs@gmail.com>
2018-10-17 20:57:31 -0300

    Salva lista

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog
 
-from src.lista import Lista
-
+# interface gráfica
 from src.form import Ui_Form
 
+# classes
+from src.lista import Lista
 
-#declarações da interface gráfica
+# declarações da interface gráfica
 app = QApplication(sys.argv)
 window = QDialog()
 ui = Ui_Form()
 ui.setupUi(window)
 
 # inicializa a janela
-#window.show()
+# window.show()
 
 print("Teste")
 Pessoa = Lista("pessoa")
-print("Nome:",Pessoa.id)
-print("Nome:",Pessoa.id[0])
-print("Nome:",Pessoa.id[0]['nome'])
-#sys.exit(app.exec_())
+print("Nome:", Pessoa.id)
+print("Nome:", Pessoa.id[0])
+print("Nome:", Pessoa.id[0]['nome'])
+Pessoa.salva()
+# sys.exit(app.exec_())
