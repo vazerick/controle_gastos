97f4e96dbceb3b88b0707bafd91180f7d8d8b995
vazerick <hp36vA12$>
2019-12-27 09:47:19 -0300

    Remove o sistema de Pessoas

--- src/lista.py
+++ src/lista.py
@@ -40,66 +40,6 @@ class Lista:
         self.id[index]['status'] = status
 
 
-class ListaPessoa(Lista):
-
-    def __init__(self, nome):
-        super().__init__(nome)
-
-        for item in self.itens:  # para cada item da lista, cria um dicionário com os atributos definidos
-            nome = item.getElementsByTagName("nome")[0]
-            status = item.getElementsByTagName("status")[0]
-            self.id.append(
-                {
-                    'id': int(item.getAttribute("id")),
-                    'nome': nome.childNodes[0].data,
-                    'status': int(status.childNodes[0].data),
-                }
-            )
-
-    def getAtivos(self):
-        ativos = []  # lista vazia para abrigar os itens ativos
-        for item in range(len(self.id)):
-            if self.id[item]["status"]:
-                ativos.append(
-                    {
-                        'id': self.id[item]["id"],
-                        'nome': self.id[item]["nome"],
-                    }
-                )
-        return ativos
-
-    def salva(self):
-        doc = minidom.Document()  # cria um objeto xml
-        # adiciona um comentário com o horário de criação
-        doc.appendChild(doc.createComment("Criacao: %s" % time.asctime(time.localtime(time.time()))))
-        lista = doc.createElement('lista')  # cria uma lista xml de doc
-        for indice in self.id:  # adiciona cada nó na lista de doc
-            item = doc.createElement('pessoa')
-            item.setAttribute('id', str(indice['id']))
-            lista.appendChild(item)
-
-            nome = doc.createElement('nome')
-            item.appendChild(nome)
-            nome.appendChild(doc.createTextNode(indice['nome']))
-
-            status = doc.createElement('status')
-            item.appendChild(status)
-            status.appendChild(doc.createTextNode(str(indice['status'])))
-
-        doc.appendChild(lista)
-
-        arquivo = open(self.endereco, "w", encoding='utf-8')
-        arquivo.write(doc.toprettyxml(indent='   '))
-
-    def getId(self, nome):
-        for item in self.id:
-            if item["nome"] == nome:
-                return item["id"]
-
-    def getNome(self, id):
-        return self.id[id]['nome']
-
-
 class ListaCategoria(Lista):
 
     def __init__(self, nome):
